67f72d91be2d4212e883ba4cc9906b51ba3f92dd
LiWanBang <584847742@qq.com>
2021-10-25 18:50:46 +0800

    add eeprom

---
+++ examples/ss881x/eeprom/doc/readme.md
@@ -0,0 +1,77 @@
+# 1. 功能说明
+​	读、写EEPROM
+
+# 2. 功能实现
+
+1.写流程： 
+
+a)  EEPCON使能 写   b)  EEPADR写入地址   c) EEPDAT放入数据  d)  EEPCON激活写周期   e) EEPCON等待写周期清0    f) 重复b-d    g) EEPCON清除读写使能控制
+
+2.读流程
+
+a)  EEPCON使能 读   b)  EEPADR写入地址   c)  EEPCON激活读周期   d) EEPCON等待写周期清0    e) EEPDAT读出数据  c) 重复b-e  d) EEPCON清除读写使能控制
+
+# 3. 代码编译
+
+## 3.1 PlatformIO IDE
+
+### 3.1.1 参考如下链接，搭建PlatformIO IDE的开发环境
+
+http://www.sinhmicro.com.cn/index.php/more/blog/vscode-platformio-sinh51
+
+### 3.1.2 在PlatformIO IDE中打开工程并编译
+
+和其它示例基本一致，不再详细说明，具体请参考：
+
+[led-blink/doc/readme.md](../../led-blink/doc/readme.md)
+
+## 3.2 Keil C51 IDE
+
+### 3.2.1 参考如下链接，搭建Keil C51 IDE的开发环境
+
+http://www.sinhmicro.com/index.php/tool/software/debugger/sinh51_keil
+
+### 3.2.2 在Keil C51 IDE中打开工程并编译
+
+和其它示例基本一致，不再详细说明，具体请参考：
+
+[led-blink/doc/readme.md](../../led-blink/doc/readme.md)
+
+# 4. 测试步骤
+
+## 4.1 通过模拟器测试
+### 4.1.1 PlatformIO IDE
+
+暂不支持。
+
+### 4.1.2 Keil C51 IDE
+暂不支持。
+
+## 4.2 通过开发板测试
+
+### 4.2.1 参考如下链接，进行硬件连接
+
+http://sinhmicro.com/index.php/tool/hardware/debugger/ssd8
+
+### 4.2.2 通过Flash_Tools烧录固件
+
+和其它示例基本一致，不再详细说明，具体请参考：
+
+[led-blink/doc/readme.md](../../led-blink/doc/readme.md)
+
+### 4.2.3 硬件调试
+
+1. 使用USB B连接线将调试器与PC连接起来。
+
+2. 点击Flash烧录软件，芯片状态下的“连接按钮”。
+
+3. 借用FLASH TOOL工具
+
+   首先屏蔽eeprom_read()函数，运行后使用输入 eeprom read 0x02 5，显示16进制数据 “hello”展示为
+
+   ![](./write.jpg)
+
+​        再屏蔽eeprom_write()函数，在调试模式运行可以看到RD的值就是读到的，也可以借助FLASH TOOL修改，观察变化
+
+![](./read.jpg)
+
---
+++ examples/ss881x/eeprom/src/eeprom.c
@@ -0,0 +1,101 @@
+/*
+ * eeprom
+ *
+ * read and write eeprom.
+ *
+ * Author: <liwanbang@sinhmicro.com.cn>
+ * 
+ * Copyright (C) 2021 Sinh Micro, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#include "ss881x.h"
+
+unsigned char WD[5] = "hello";
+unsigned char RD[5] = "     ";  
+
+void eeprom_write_byte(unsigned char addr,unsigned char byt)
+{
+        
+        EFRADR = 0x00;
+        EFRDAT = addr;          /* set addr */
+        
+        EFRADR = 0x01;
+        EFRDAT = byt;           /* set dat */
+        
+        EFRADR = 0x02;
+        EFRDAT |= (1<<3);       /* start write */
+        
+        EFRADR = 0x02;
+        while(EFRDAT&(1<<3));   /* wait finish */
+}
+
+void eeprom_write(unsigned char addr,unsigned char *dat,unsigned char num)
+{
+        unsigned char i=0;
+        
+        EFRADR = 0x02;
+        EFRDAT |= 0x02;         /* enable write */
+        
+        while(i<num){
+                eeprom_write_byte(addr+i,dat[i]);
+                i++;
+        }
+        EFRDAT &= ~0x03;        /* clear write */
+}
+
+unsigned char eeprom_read_byte(unsigned char addr)
+{
+        unsigned char dat=0;
+        
+        EFRADR = 0x00;
+        EFRDAT = addr;          /* set addr */
+        
+        
+        EFRADR = 0x02;
+        EFRDAT |= (1<<2);       /* start read */
+        
+        EFRADR = 0x02;
+        while(EFRDAT&(1<<2));   /* wait finish */
+        
+        EFRADR = 0x01;
+        dat = EFRDAT;
+        
+        return dat;
+}
+
+void eeprom_read(unsigned char addr,unsigned char *date,unsigned char num)
+{       
+        unsigned char i=0;
+
+        EFRADR = 0x02;
+        EFRDAT |= 0x01;         /* enable read */
+        
+        while(i<num){
+                *(date+i) = eeprom_read_byte(addr+i);
+                i++;
+        }
+        EFRDAT &= ~0x03;        /* clear write */
+}
+
+void main()
+{       
+        WDTCON = 0x05;                           /* ¹Ø±Õ¿´ÃÅ¹· */
+        
+        eeprom_write(0x02,WD,5);
+        eeprom_read(0x02,RD,5);   /* eeprom read 0x02 5 */
+        while (1){
+			
+        }
+}
